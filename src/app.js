b7cb433ab4d3b4066d1962c4c64883b3133e8fd2
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2020-02-27 18:33:34 +0200

    the get method works just fine

---
+++ src/app.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+import './style.scss';
+
+import Message from './components/message.js';
+import Form from './components/form.js';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <Form/>
+    );
+  }
+}
+
+export default App;
